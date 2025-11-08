337ab02cbe8448483e93507211147372fca29c36
WIN11 <adnan.codesymphony@gmail.com>
2025-11-08 12:25:49 +0530

    Find , FindOne, Update, Delete in Product Table, CRUD Completed

--- nest-project/src/product/product.controller.ts
+++ nest-project/src/product/product.controller.ts
@@ -1,12 +1,46 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    this.productService.create(createProductDto);
+    return this.productService.create(createProductDto);
+  }
+
+  @Get()
+  find() {
+    return this.productService.find();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    await this.productService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    await this.productService.delete(id);
   }
 }
--- nest-project/src/product/product.service.ts
+++ nest-project/src/product/product.service.ts
@@ -3,17 +3,45 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product) private productRepository: Repository<Product>,
   ) {}
+
   create(productDetails: CreateProductDto) {
-    const newproduct = new Product();
-    newproduct.pname = productDetails.pname;
-    newproduct.pdesc = productDetails.pdesc;
-    newproduct.pprice = productDetails.pprice;
-    this.productRepository.save(newproduct);
+    const newproduct = { ...new Product(), ...productDetails };
+    return this.productRepository.save(newproduct);
+  }
+
+  find() {
+    return this.productRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.productRepository.find({ where: { pid: id } });
+  }
+
+  update(id: number, updateDetails: UpdateProductDto) {
+    const updateProduct = { ...updateDetails };
+    return this.productRepository.update(id, updateProduct);
+  }
+
+  delete(id: number) {
+    return this.productRepository.delete(id);
   }
 }
+
+// const newproduct = new Product();
+// newproduct.pname = productDetails.pname;
+// newproduct.pdesc = productDetails.pdesc;
+// newproduct.pprice = productDetails.pprice;
+
+//---------------------------------------------------
+// const updateProduct = {
+//       pname: updateDetails.pname,
+//       pdesc: updateDetails.pdesc,
+//       pprice: updateDetails.pprice,
+//     };
