ee06873890675cf90fd7e58a42dca8bb6f99d4ef
WIN11 <adnan.codesymphony@gmail.com>
2025-11-26 19:11:31 +0530

    Class Component, Functional Component

---
+++ Frontend/MyReactApp/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ClassComponent from './components/ClassComponent'
+import FunctionalComponent from './components/FunctionalComponent'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1>Welcome to React.js</h1>
+          <ClassComponent />
+          <FunctionalComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
